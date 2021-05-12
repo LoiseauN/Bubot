b23a7a8fb1afb1106750d4f7571a9bb678423d91
Nicolas Loiseau <nloiseau@iMac-de-Nicolas.local>
2021-05-12 16:12:51 +0200

    :fish: Figure functional ok!

--- R/10_PlotDiversity.R
+++ R/10_PlotDiversity.R
@@ -70,23 +70,10 @@ c <- ggplot(alpha_div_all, aes(x=classDepth, y=alpha_hill_fonct_richess)) +
 d <- ggplot(alpha_div_all, aes(x=classDepth, y=alpha_hill_fonct_entropy)) + 
   geom_boxplot(fill ="cadetblue3",pch=21)+
   theme_bw()+ylab("Alpha-entropy fonct")+ geom_jitter(width = 0.1, size = 0.5)
- 
-e <- ggplot(alpha_div_all, aes(x=classDepth, y=alpha_hill_phylo_richess)) + 
-  geom_boxplot(fill ="cadetblue3",pch=21)+
-  theme_bw()+ylab("Alpha-richness phylo")+ geom_jitter(width = 0.1, size = 0.5)
- 
-f <- ggplot(alpha_div_all, aes(x=classDepth, y=alpha_hill_phylo_entropy)) + 
-  geom_boxplot(fill ="cadetblue3",pch=21)+
-  theme_bw()+ylab("Alpha-entropy phylo")+ geom_jitter(width = 0.1, size = 0.5)
- 
+
 title <- textGrob("Alpha Hill boxplot",
                   gp=gpar(fontsize=20,fontface=2))
-grid.arrange(a,b,c,d,e,f,ncol=2,top = title)
-
-
-
-
-
+grid.arrange(a,b,c,d,ncol=2,top = title)
 
 
 
@@ -112,20 +99,9 @@ d <- ggplot(beta_hill, aes(x=diff_depth, y=beta_hill_fonct_entropy)) +
   theme_bw()+ylab("Beta-entropy taxo")+
   geom_smooth(method = lm,formula = y ~ splines::bs(x, 2),colour="orange",fill="orange")
 
-e <- ggplot(beta_hill, aes(x=diff_depth, y=beta_hill_phylo_richess)) + 
-  geom_point(fill ="cadetblue3",pch=21)+xlim(0,max(beta_hill$diff_depth))+
-  theme_bw()+ylab("Beta-richness taxo")+
-  geom_smooth(method = lm,formula = y ~ splines::bs(x, 2),colour="orange",fill="orange")
-
-f <- ggplot(beta_hill, aes(x=diff_depth, y=beta_hill_phylo_entropy)) + 
-  geom_point(fill ="cadetblue3",pch=21)+xlim(0,max(beta_hill$diff_depth))+
-  theme_bw()+ylab("Beta-entropy taxo")+
-  geom_smooth(method = lm,formula = y ~ splines::bs(x, 2),colour="orange",fill="orange")
-
-
 title <- textGrob("Beta Hill",
                   gp=gpar(fontsize=20,fontface=2))
-grid.arrange(a,b,c,d,e,f,ncol=2,top = title)
+grid.arrange(a,b,c,d,ncol=2,top = title)
 
 
 ###PLot Distance decay
@@ -189,3 +165,203 @@ ggplot(ResHill, aes(x=depth, y=phylo_entro_m)) +
                 position=position_dodge(0.05),color ="cadetblue3")+
   theme_bw()+ylab("Beta-entropy phylo")+
   geom_smooth(method = lm,formula = y ~ splines::bs(x, 2),colour="orange",fill="orange")
+
+
+
+
+
+
+###Plot Functional Space
+biomass_mat_classDepth = reshape2::melt(dat_complet , id.vars = c( "classDepth" , "species" ) , measure.vars = "Groupweigth")
+biomass_mat_classDepth = reshape2::dcast( biomass_mat_classDepth , classDepth~species,sum,na.rm=T)
+rownames(biomass_mat_classDepth) <- biomass_mat_classDepth[,1]
+biomass_mat_classDepth <- biomass_mat_classDepth[,-1]
+
+#Remove site with not enough species 
+preabs_mat_classDepth <- biomass_mat_classDepth
+preabs_mat_classDepth[preabs_mat_classDepth>0] <- 1
+preabs_mat_classDepth <- preabs_mat_classDepth[apply(preabs_mat_classDepth,1,sum)>4,]
+preabs_mat_classDepth <- preabs_mat_classDepth[,apply(preabs_mat_classDepth,2,sum)>0]
+biomass_mat_classDepth <- biomass_mat_classDepth[rownames(biomass_mat_classDepth) %in% rownames(preabs_mat_classDepth),]
+biomass_mat_classDepth <- biomass_mat_classDepth[,colnames(biomass_mat_classDepth) %in% colnames(preabs_mat_classDepth)]
+
+#Traits matrix
+trait_mat <- dat_complet[,c("species","mobility","activity","schooling","position","clean_diet",
+                            "maxLengthTL_Fishbase")]
+trait_mat <-unique(trait_mat)
+
+rownames(trait_mat)  <- trait_mat[,1]
+
+trait_mat  <- trait_mat[,-1]
+
+trait_mat <- trait_mat[rownames(trait_mat) %in% colnames(biomass_mat_classDepth),]
+
+trait_cat <- data.frame(trait_name = colnames(trait_mat),
+                        trait_type = c("O","N","O","O","O","Q")
+)
+
+traits_summary <- mFD::sp.tr.summary(tr_cat = trait_cat,    # Traits informations
+                                     sp_tr  = trait_mat, stop_if_NA = TRUE)
+#Traits distance     
+sp_dist_traits <-  mFD::funct.dist(sp_tr         = trait_mat,
+                                   tr_cat        = trait_cat,
+                                   metric        = "gower",
+                                   scale_euclid  = "scale_center",
+                                   ordinal_var   = "classic",
+                                   weight_type   = "equal",
+                                   stop_if_NA    = TRUE)
+
+#Traits space quality    #4 dimension
+fspaces_quality <- mFD::quality.fspaces(sp_dist             = sp_dist_traits,
+                                        maxdim_pcoa         = 10,
+                                        deviation_weighting = "absolute",
+                                        fdist_scaling       = FALSE,
+                                        fdendro             = "average")
+
+#Correlation Traits PCOA
+sp_faxes_coord <- fspaces_quality$"details_fspaces"$"sp_pc_coord"
+tr_faxes <- mFD::traits.faxes.cor(sp_tr = trait_mat, 
+                                  sp_faxes_coord = sp_faxes_coord[, c("PC1", "PC2", "PC3", "PC4")], plot = TRUE)
+
+tr_faxes$tr_faxes_plot
+
+
+#Compute Functional diversity
+alpha_fd_indices <- mFD::alpha.fd.multidim(sp_faxes_coord[, c("PC1", "PC2", "PC3", "PC4")],
+                                           asb_sp_w      = as.matrix(biomass_mat_classDepth),
+                                           ind_vect      = c("fdis", "fmpd", "fnnd", "feve", "fric", "fdiv", "fori", "fspe"),
+                                           scaling       = TRUE,
+                                           check_input   = TRUE,
+                                           details_returned = TRUE)
+
+
+# Seb illustrating FD with new mFD package
+# link (not to share at this point, software paper submitted to Ecography)
+# https://cmlmagneville.github.io/mFD/
+# 48 warning messages not a problem (points not drawn)
+
+
+# libraries ---
+library(tidyverse)
+library(mFD)
+
+
+# color code for depth ----
+
+hab_depth<-c("[0-20[","[20-40[","[40-60[",
+             "[60-80[",">80")
+
+vcolors <- col2rgb(c("cyan2","deepskyblue",
+                     "deepskyblue3","dodgerblue3","dodgerblue4"))
+
+
+vcolors <- apply(vcolors, 2, function(x) {
+  rgb(red = x[1], green = x[2], blue = x[3], maxColorValue = 255)
+})
+
+
+
+
+names(vcolors)<-hab_depth
+
+# coordinates of all species
+pool_coord<-alpha_fd_indices$details$sp_faxes_coord
+
+# vertices of all fe in 4D ----
+pool_vert_nm<-alpha_fd_indices$details$pool_vert_nm
+
+# range of axes
+range_faxes_coord <- range(pool_coord[,1:4])
+range_axes <- range_faxes_coord +
+  c(-1, 1) * (range_faxes_coord[2] - range_faxes_coord[1]) * 0.1
+
+# indices values
+hab_depth_fd<-alpha_fd_indices$functional_diversity_indices
+
+
+## plotting along pairs of axes ####
+
+pairs_axes<-list(c(1,2))#, c(3,4))
+
+for (z in 1:length(pairs_axes))
+{
+  
+  # names of axes   
+  xy<-pairs_axes[[z]]
+  
+  # list to store ggplot
+  ggplot_list<-list()
+  
+  
+  for (v in hab_depth ) {
+    # v="[0-20["
+    
+    # color for habitat*pH levels
+    col_v<-as.character(vcolors[v])
+    
+    # species present in v
+    sp_v<-names(which(alpha_fd_indices$details$asb_sp_occ[v,]==1))
+    
+    # background with axes range set + title
+    ggplot_v<-background.plot(range_faxes=range_axes,
+                              faxes_nm=paste0("PC", xy), 
+                              color_bg="#FFA5001E")
+    ggplot_v<-ggplot_v + labs(subtitle=v)
+    
+    # convex hull of species pool
+    ggplot_v<-pool.plot(ggplot_bg=ggplot_v,
+                        sp_coord2D=pool_coord[,xy],
+                        vertices_nD=pool_vert_nm,
+                        plot_pool=FALSE,
+                        color_ch=NA, fill_ch="white", alpha_ch=1
+    )
+    
+    # plot convex hull of assemblage but not species
+    ggplot_v<-fric.plot( ggplot_bg=ggplot_v, 
+                         asb_sp_coord2D=list(vv=pool_coord[sp_v,xy]),
+                         asb_vertices_nD=list(vv=alpha_fd_indices$details$asb_vert_nm[[v]]),
+                         plot_sp = FALSE,
+                         color_ch=c(vv=col_v),
+                         fill_ch=c(vv=col_v),
+                         alpha_ch=c(vv=0.1)
+    )
+    
+    
+    # plot species weights using plot.fide without showing mean value
+    ggplot_v<- fide.plot(ggplot_bg=ggplot_v,
+                         asb_sp_coord2D=list(vv=pool_coord[sp_v,xy]),
+                         asb_sp_relatw=list(vv=alpha_fd_indices$details$asb_sp_relatw[v,sp_v]),
+                         asb_fide_coord2D=list(vv=hab_depth_fd[v,paste0("fide_PC", xy)]),
+                         plot_sp = TRUE,
+                         shape_sp = c(vv=21),
+                         color_sp =c(vv=col_v),
+                         fill_sp = c(vv=paste0(col_v,"70") ),
+                         shape_fide = c(vv=23),
+                         size_fide = c(vv=1),
+                         color_fide = c(vv=col_v),
+                         fill_fide = c(vv=col_v),
+                         color_segment = c(vv=col_v),
+                         width_segment = c(vv=0.5),
+                         linetype_segment = c(vv=1)
+    )
+    
+    
+    # ggplot_v storing in list
+    ggplot_list[[v]]<-ggplot_v
+    
+  }# end of v
+  
+  
+  # patchwork of plots : 4 habitats (rows) * 3 columns (pH)
+  FD_xy<- (ggplot_list[[1]] + ggplot_list[[2]] + ggplot_list[[3]] ) / 
+    (ggplot_list[[4]] + ggplot_list[[5]])
+  
+  
+  # saving as png ----
+  sz<-3
+  #ggsave(FD_xy, filename = paste0("FD_pc",xy[1],"vs",xy[2],".png"), 
+  #      device = "png", width=3*sz, height=4*sz)
+  
+  
+}# end of axes_id
+FD_xy
